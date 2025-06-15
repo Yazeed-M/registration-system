5588c327651b0910aada7440ec5b4a885d8e6db8
yazeed <yazeedj01@gmail.com>
2025-06-15 13:37:23 +0300

    set up elide for users

---
+++ src/main/java/com/project/registration_system/configs/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.project.registration_system.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authz -> authz
+                                .anyRequest().permitAll()
+                )
+                .httpBasic(basic -> basic.disable())
+                .formLogin(login -> login.disable());
+
+        return http.build();
+    }
+}
